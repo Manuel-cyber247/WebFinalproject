616c975e769ac62243f77e01388612acdfa3a041
Manuel-cyber247 <ufoarohchinecherem@gmail.com>
2025-09-30 21:43:51 +0100

    Second commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+import focalboard_image from '../Assets/focalboard_image.png';
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className='brand'>
+          
+          <h1><a href="http://"><img src={focalboard_image} alt="Focalboard Logo" />Focalboard</a></h1>
+        </div>
+      <ul>
+        <li><a href="#">Contribute</a></li>
+        <li><a href="#">Github</a></li>
+        <li><a href="#">Help Docs</a></li>
+        <li><a href="#">Feedback</a></li>
+        <li><a href="#">Download</a></li>
+        <li><button class="nav-btn">Get Started</button></li>
+      </ul>
+      
+    </div>
+  );
+}
+export default Navbar;
